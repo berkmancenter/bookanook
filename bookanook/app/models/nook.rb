f939c27ffadab77db9853a882b05d72821f819ef
jdcc <jclark@cyber.law.harvard.edu>
2015-06-25 17:47:29 -0400

    validations and json

--- bookanook/app/models/nook.rb
+++ bookanook/app/models/nook.rb
@@ -5,6 +5,19 @@ class Nook < ActiveRecord::Base
   belongs_to :manager, class_name: 'User', foreign_key: 'user_id'
   has_many :reservations
 
+  serialize :attrs, JSON
+  serialize :hidden_attrs, JSON
+
+  validates_presence_of :name, :location_id
+  # schedulable and reservation_length are both in seconds
+  validates_numericality_of :min_capacity, :min_schedulable,
+    :min_reservation_length, greater_than_or_equal_to: 0
+  validates_numericality_of :min_capacity, :max_capacity, only_integer: true
+  validates_inclusion_of :bookable, :repeatable, :requires_approval,
+    in: [true, false]
+
+  after_initialize :set_defaults
+
   mount_uploaders :photos, PhotoUploader
 
   searchable do
@@ -17,11 +30,18 @@ class Nook < ActiveRecord::Base
          join: { from: :nook_id, to: :id }, as: 'start_d')
   end
 
-  def status
-    'Available'
-  end
-
   private
+
+  def set_defaults
+    self.hours ||= {}
+    self.min_capacity ||= 0
+    self.min_schedulable ||= 0
+    self.min_reservation_length ||= 0
+    self.attrs ||= {}
+    self.hidden_attrs ||= {}
+    self.bookable ||= true if bookable.nil?
+    self.repeatable ||= false if repeatable.nil?
+  end
 
   def self.inheritance_column
     :sti_type
--- bookanook/app/models/reservation.rb
+++ bookanook/app/models/reservation.rb
@@ -2,10 +2,43 @@ class Reservation < ActiveRecord::Base
   belongs_to :nook
   belongs_to :requester, class_name: 'User', foreign_key: 'user_id'
 
+  module Status
+    PENDING, REJECTED, CONFIRMED, CANCELED =
+      'Awaiting review', 'Rejected', 'Confirmed', 'Canceled'
+  end
+
+  # From active_support/core_ext/numeric/time.rb
+  REPEATABLE_UNITS = [
+    'second', 'seconds',
+    'minute', 'minutes',
+    'hour', 'hours',
+    'day', 'days',
+    'week', 'weeks',
+    'fortnight', 'fortnights',
+  ]
+
+  serialize :repeats_every
+
+  validates_presence_of :start, :end
+  validates_inclusion_of :status, in: Status.constants 
+  validates_inclusion_of :public, in: [true, false]
+  validates_numericality_of :priority, only_integer: true,
+    greater_than_or_equal_to: 0
+
+  after_initialize :set_defaults
+
   searchable do
     integer :nook_id
     string :status
     time :start
     time :end
+  end
+
+  private
+
+  def set_defaults
+    self.public ||= true if self.public.nil?
+    self.status ||= Status::PENDING
+    self.priority ||= 0
   end
 end
