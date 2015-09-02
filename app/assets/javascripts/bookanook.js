23cb27a9425126bef32608b419d67dd045d35cd4
Kent K <kent@endpoint.com>
2015-09-02 12:38:35 -0700

    Begin modifying Bootstrap behaviors/appearances to match the mockup better.
    Couple of other behind the scenes cleanups.

--- app/assets/javascripts/bookanook.js
+++ app/assets/javascripts/bookanook.js
@@ -19,12 +19,40 @@ $(function() {
     }
   });
 
+  $(document).on('click', '.dropdown-menu input, .dropdown-menu label, .dropdown-menu label li', function(e){
+    e.stopPropagation();
+  });
+
+  $(document).on('shown.bs.dropdown', '.left-menu .dropdown', function() {
+    $(this).data('closable', false);
+  });
+
+  $(document).on('click', '.left-menu .dropdown a', function() {
+    if($(this).data('clicked')) {
+      var d = $(this).closest('.dropdown');
+      d.data('closable', true); d.trigger('force-hide');
+    } else {
+      $(this).data('clicked', true);
+    }
+  });
+
+  $(document).on('hide.bs.dropdown', '.left-menu .dropdown', function() {
+    return $(this).data('closable');
+  });
+
+  $(document).on('force-hide', '.left-menu .dropdown', function() {
+    $(this).removeClass('open');
+    $(this).find('.dropdown-toggle').data('clicked', false);
+  });
+
   $('.datepicker-element').datepicker();
+
   $(".datepicker-element").on("changeDate", function(event) {
     $(event.target).parent().find('input').val(
       $(event.target).datepicker('getFormattedDate')
     )
   });
+
   $(".datepicker-element").on("hide", function(event) {
     console.log('foo');
   });
