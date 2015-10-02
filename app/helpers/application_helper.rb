9a75d8c0bfd1010f1ed80f683a069954bdb8eb47
jdcc <jclark@cyber.law.harvard.edu>
2015-10-02 12:15:27 -0400

    add flashes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,10 @@ module ApplicationHelper
   def active_page?(page)
     params[:controller] == page
   end
+
+  def alert_class(flash_name)
+    return 'alert-danger' if flash_name == 'alert'
+    return 'alert-success' if flash_name == 'notice'
+    return 'alert-info' if flash_name == 'flash'
+  end
 end
