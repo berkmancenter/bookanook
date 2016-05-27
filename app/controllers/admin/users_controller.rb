d4c597865a34bcaccf8bd0fe1a3701e4ab05768a
Shubham Patel <shubhpatel108@gmail.com>
2016-05-27 09:46:11 +0530

    Implement #103: Allow superadmin to make another User superadmin

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -18,6 +18,54 @@ module Admin
 
     before_filter :authorize_index_access, only: :index
 
+    def create
+      create_params = resource_params
+      create_params.delete(:is_superadmin)
+      resource = resource_class.new(create_params)
+
+      if resource.save
+        if params[:is_superadmin]
+          resource.add_role :superadmin
+        else
+          resource.remove_role :superadmin
+        end
+        redirect_to(
+          [namespace, resource],
+          notice: translate_with_resource("create.success"),
+        )
+      else
+        render :new, locals: {
+          page: Administrate::Page::Form.new(dashboard, resource),
+        }
+      end
+    end
+
+    def edit
+      @is_superadmin = requested_resource.has_role? :superadmin
+      super
+    end
+
+    def update
+      update_params = resource_params
+      update_params.delete(:is_superadmin)
+
+      if requested_resource.update(update_params)
+        if params[:is_superadmin]
+          requested_resource.add_role :superadmin
+        else
+          requested_resource.remove_role :superadmin
+        end
+        redirect_to(
+          [namespace, requested_resource],
+          notice: translate_with_resource("update.success"),
+        )
+      else
+        render :edit, locals: {
+          page: Administrate::Page::Form.new(dashboard, requested_resource),
+        }
+      end
+    end
+
     private
     def authorize_index_access
       authorize! :read, User
