75e2b50629b5258bcf61fca74835d0414dfff002
Shubham Patel <shubhpatel108@gmail.com>
2016-06-04 11:59:14 +0530

    Notify user about earlier registration when trying to register with Google

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,13 +1,23 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   def google_oauth2
-    @user = GoogleUser.find_or_create(request.env['omniauth.auth'])
+    email = request.env['omniauth.auth']['info']['email']
+    @user = User.where(email: email).first
 
-    if @user.persisted?
+    if @user.nil?
+      @user = GoogleUser.find_or_create(request.env['omniauth.auth'])
+      if @user.persisted?
+        flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind: 'Google'
+        sign_in_and_redirect @user, event: :authentication
+      else
+        session['devise.google_data'] = request.env['omniauth.auth']
+        redirect_to new_user_registration_url
+      end
+    elsif not @user.type.nil? and @user.type == 'GoogleUser'
       flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind: 'Google'
       sign_in_and_redirect @user, event: :authentication
     else
-      session['devise.google_data'] = request.env['omniauth.auth']
+      flash[:alert] = I18n.t 'users.authentication.simple_registration'
       redirect_to new_user_registration_url
     end
   end
