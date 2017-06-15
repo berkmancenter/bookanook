4ce7d5e2d9678cd1b5b4d7b52d8955969eb67299
Gaurav Koley <arkokoley@live.in>
2017-06-15 23:57:14 +0530

    Update tests for issue #19

--- spec/requests/timezones_spec.rb
+++ spec/requests/timezones_spec.rb
@@ -10,15 +10,27 @@ RSpec.describe "Timezone Set", type: :request do
     end
 
     it "should correctly set admin timezone" do
-      FactoryGirl.create :user
-      user = User.last
-      user.add_role :admin
-      user.confirmed_at = Time.zone.now
-      user.save
+      user = create :admin
       sign_in user
       get admin_root_url
       assert_equal 200, status
       expect(response.body).to include(cookies["browser.timezone"])
     end
+
+    it "should display available bookable slots in user timezone" do
+      user = create :confirmed_user
+      sign_in user
+      @nook = create :nook
+      xhr :get, reserve_nook_url(@nook), search_date: Date.today
+      time_at_10 = Time.zone.parse("10:00").in_time_zone(cookies["browser.timezone"])
+      time_at_530 = Time.zone.parse("5:30").in_time_zone(cookies["browser.timezone"])
+      if time_at_10.min == 0
+        expect(response.body).to include('<button class="btn open" type="button" value="' + time_at_10.strftime("%H%M") + '"></button>')
+      else
+        expect(response.body).to include('<button class="btn open right" type="button" value="' + time_at_10.strftime("%H%M") + '"></button>')
+      end
+      expect(response.body).to include('<button class="btn taken" disabled="true" type="button" value="' + time_at_530.strftime("%H%M") + '"></button>')
+    end
+
   end
 end
