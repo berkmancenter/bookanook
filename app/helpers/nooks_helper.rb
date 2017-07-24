68e555d6e5ca1d75489d21938269aa39d28a856d
Gaurav Koley <arkokoley@live.in>
2017-07-24 23:23:08 +0530

    Search for nooks by specifying capacity
    Completes issue #123

--- app/helpers/nooks_helper.rb
+++ app/helpers/nooks_helper.rb
@@ -1,2 +1,6 @@
 module NooksHelper
+  def nook_capacity
+    array = (Nook.minimum(:min_capacity) || 0)..(Nook.maximum(:max_capacity) || 0)
+    return array.map{ |x| x > 0 ? [x, x] : [x, "Any"] }.to_h
+  end
 end
